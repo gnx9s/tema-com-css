be023e1e8e41758cdebf67a254da6c7670e5ed7d
arielcollatti <gabriel.rodriguesm0rg@outlook.com>
2023-08-30 00:12:05 -0300

    arrumar as transitions

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('modonoturno');
         container.classList.toggle('modonoturno');
 
-        if(body.classList.contains('modonoturno')) {
+        if(body.classList.contains('modonoturno')) 
+        {
             body.style.background = '#142d4c';
             body.style.backgroundColor = '#091727';
             container.style.border = 'solid #081b31'
             container.style.boxShadow = '8px 8px 0 0 #050d18'
         }
 
-        else{
+        else
+        {
             body.style.background = 'linear-gradient(45deg, #e0c3fc 1%, #8ec5fc 100%)';
             container.style.border = 'solid #cadefc'
             container.style.boxShadow = '8px 8px 0 0 #c3bef0';
